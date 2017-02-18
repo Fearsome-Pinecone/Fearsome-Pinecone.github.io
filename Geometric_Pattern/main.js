f038d36b17e26f31706f6f8c4a6636d8fa9f3c99
Fearsome_Pinecone <18gsmith@students.bowmancharter.org>
2017-02-18 22:09:14 +0000

    Offset circles in geometric_pattern

---
+++ Geometric_Pattern/main.js
@@ -0,0 +1,33 @@
+var NUM_CIRCLES =12;
+var circleDiameter;
+var circleRadius;
+
+function setup() {
+  createCanvas (480,600);
+  circleDiameter = width/NUM_CIRCLES;
+  circleRadius = circleDiameter/2;
+}
+
+function draw() {
+ var isShifted = false;
+
+  var y = height;
+  while (y >= 0) {
+
+    var x;
+
+    if (isShifted) {
+      x = circleRadius;
+    } else {
+      x = 0;
+    }
+
+    while (x <= width) {
+      ellipse(x, y, circleDiameter, circleDiameter);
+      x = x + circleDiameter;
+    }
+
+    y = y - circleRadius;
+    isShifted = !isShifted;
+  }
+}
